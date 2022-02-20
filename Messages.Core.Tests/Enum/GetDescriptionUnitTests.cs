46c2622998224dac14d7adf32d6e4872e68047a1
Leonardo de Oliveira <leo.oliveira.eng@outlook.com>
2022-02-20 01:26:36 -0300

    #66 - Implementing unit tests for get description

---
+++ Messages.Core.Tests/Enum/GetDescriptionUnitTests.cs
@@ -0,0 +1,41 @@
+﻿using FluentAssertions;
+using Messages.Core.Enums;
+using Messages.Core.Enums.Extensions;
+using Messages.Core.Tests.Shared;
+using Xunit;
+
+namespace Messages.Core.Tests.Enum
+{
+    public class GetDescriptionUnitTests : BaseMock
+    {
+        [Fact]
+        public void GetDescription_ShouldReturnEmptyString_FieldInfoIsNull()
+        {
+            var messageType = (MessageType)int.MaxValue;
+
+            var response = messageType.GetDescription();
+
+            response.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void GetDescription_ShouldReturnEnumToString_NoCustomAttributes()
+        {
+            var testEnum = AnyEnum.Any;
+
+            var response = testEnum.GetDescription();
+
+            response.Should().Be("Any");
+        }
+
+        [Fact]
+        public void GetDescription_ShouldReturnEnumDescription_CustomAttibuteDefined()
+        {
+            var testEnum = AnyEnum.None;
+
+            var response = testEnum.GetDescription();
+
+            response.Should().Be("Undefined");
+        }
+    }
+}
